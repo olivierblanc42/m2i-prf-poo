e7b99b9dfa93a6e0965ba2f324d14b0bf254bd90
olivierblanc42 <olivierhumanbooster@gmail.com>
2024-05-02 12:22:40 +0200

    add index

---
+++ 1-composer/index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test de l'autoloading PSR-4</title>
+</head>
+
+<body>
+
+    <h1>Page de test</h1>
+
+    <h2>Fiche détaillée d'un membre :</h2>
+    
+   
+
+</body>
+</html>
